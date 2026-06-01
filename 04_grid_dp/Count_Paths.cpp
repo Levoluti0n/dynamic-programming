8fb32bc6a694104766c5845c03fccb7e074366e4
Levon <levon.mezhlumyan.h@gmail.com>
2026-06-01 16:34:46 +0400

    Add unique paths combinatorics

--- 04_grid_dp/Count_Paths.cpp
+++ 04_grid_dp/Count_Paths.cpp
@@ -49,3 +49,18 @@ int uniquePaths(int m, int n) {
     }
     return dp[n-1];
 }
+
+// Combinatorics
+
+int uniquePaths(int m, int n) {
+    long long res = 1;
+
+    int N = m + n - 2;
+    int r = min(m - 1, n - 1);
+
+    for (int i = 1; i <= r; ++i) {
+        res = res * (N - r + i) / i;
+    }
+
+    return (int)res;
+}
