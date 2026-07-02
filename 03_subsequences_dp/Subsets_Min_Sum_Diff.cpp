2688482762db4388e4b4896475d4bb7fe8cf1c23
Levon <levon.mezhlumyan.h@gmail.com>
2026-07-02 12:45:45 +0400

    Optimize Minimum Difference of 2 Subsets

--- 03_subsequences_dp/Subsets_Min_Sum_Diff.cpp
+++ 03_subsequences_dp/Subsets_Min_Sum_Diff.cpp
@@ -12,82 +12,70 @@ bool solve(int i, int t, vector<int>& nums, vector<vector<int>>& dp) {
 
     return dp[i][t] =
         solve(i - 1, t, nums, dp) ||
-        (t >= nums[i] ? solve(i - 1, t - nums[i], nums, dp) : false);
+        (t >= nums[i] && solve(i - 1, t - nums[i], nums, dp));
 }
 
 int subsets_min_diff(vector<int>& nums) {
     int n = nums.size();
     int total_sum = accumulate(nums.begin(), nums.end(), 0);
-    int k = total_sum;
+    int k = total_sum / 2;
 
-    vector<vector<int>> dp(n, vector<int>(k / 2 + 1, -1));
+    vector<vector<int>> dp(n, vector<int>(k + 1, -1));
 
-    for (int t = 0; t <= k / 2; t++) {
-        solve(n - 1, t, nums, dp);
+    for (int t{k}; t >= 0; --t) {
+        if(solve(n - 1, t, nums, dp))
+            return total_sum - 2 * t;
     }
 
-    int res = INT_MAX;
-    for (int i = 0; i <= k / 2; i++) {
-        if (dp[n - 1][i]) {
-            res = min(res, k - 2 * i);
-        }
-    }
-
-    return res;
+    return 0;
 }
 
 // Tabulation
 
 int subsets_min_diff(vector<int>& nums) {
     int n = nums.size();
-    int total_sum = accumulate(nums.begin(), nums.end(), 0);
-    int k = total_sum;
+    int total = accumulate(nums.begin(), nums.end(), 0);
+    int k = total / 2;
 
-    vector<vector<bool>> dp(n, vector<bool>(k / 2 + 1, false));
+    vector<vector<bool>> dp(n, vector<bool>(k + 1, false));
 
-    for(int i{}; i < n; ++i) dp[i][0] = true;
-    if(k/2 >= nums[0]) dp[0][nums[0]] = true;
-    
-    for(int i{1}; i < n; ++i) {
-        for(int target{1}; target <= k/2; ++target) {
-            dp[i][target] = dp[i-1][target] || (target >= nums[i] && dp[i-1][target-nums[i]]);
-        }
+    dp[0][0] = true;
+    if (nums[0] <= k)
+        dp[0][nums[0]] = true;
+
+    for (int i = 1; i < n; ++i) {
+        for (int t = 0; t <= k; ++t)
+            dp[i][t] = dp[i - 1][t] || (t >= nums[i] && dp[i - 1][t - nums[i]]);
     }
 
-    int res = INT_MAX;
-    for (int i = 0; i <= k / 2; i++) {
-        if (dp[n - 1][i]) {
-            res = min(res, k - 2 * i);
-        }
+    for (int t = k; t >= 0; --t) {
+        if (dp[n - 1][t])
+            return total - 2 * t;
     }
 
-    return res;
+    return 0;
 }
 
 // Tabulation Space Optimized
 
-
 int subsets_min_diff(vector<int>& nums) {
     int n = nums.size();
     int total_sum = accumulate(nums.begin(), nums.end(), 0);
-    int k = total_sum;
+    int k = total_sum/2;
 
-    vector<bool> dp(k / 2 + 1, false);
+    vector<bool> dp(k + 1, false);
 
     dp[0] = true;
     
     for(auto num : nums) {
-        for(int target{k/2}; target >= num; --target) {
+        for(int target{k}; target >= num; --target) {
             dp[target] = dp[target] || dp[target-num];
         }
     }
 
-    int res = INT_MAX;
-    for (int i = 0; i <= k / 2; i++) {
-        if (dp[i]) {
-            res = min(res, k - 2 * i);
-        }
+    for (int t{k}; t >= 0; --t) {
+        if (dp[t]) return total_sum - 2 * t;
     }
 
-    return res;
+    return 0;
 }
